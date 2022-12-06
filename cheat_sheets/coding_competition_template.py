786ca91b7c31abf888837ad66a5017b6a320d4a1
cjteclab <cjteclab@gmail.com>
2022-12-06 18:33:42 +0100

    create coding_competition_template.py

---
+++ cheat_sheets/coding_competition_template.py
@@ -0,0 +1,51 @@
+"""Template for Coding Competitions.
+
+This template can be use for problems at coding competition.
+
+The template is structured as followd:
+    1. Docstring:
+        - can be use to collect informations about the problem.
+        - serves as notepad
+        - using doctests to test the code
+    2. Input Handling:
+        - different ways how to handle inputs
+    3. Main Function:
+        - a main function to execute the code
+"""
+
+# 1. Docstring
+
+"""Docstring:
+1. Read the problem and make notes.
+
+Notes:
+    1.
+    2.
+    3.
+
+2. Create examples and think about edge cases.
+    - Use available examples from the problem set.
+
+3. Write doctests.
+"""
+
+# 2. Input Handling
+
+"""Input Handling:
+    - Way 1:
+
+    - Way 2:
+
+    - Way 3:
+
+"""
+
+# 3. Main Function
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
